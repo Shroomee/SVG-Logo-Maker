3b59c4f6a41a7e66c4fca05f4a2cc0b0304d8673
unknown <dudeomous@gmail.com>
2023-05-31 20:12:47 -0400

    added shapes class and psuedocode

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,17 @@
 // require npms
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+//deconstructing objects
+const {Circle, Square, Triangle} = require('./lib/shapes');
 //objects
+
+
+//questions for user
+
+//generate logo function
+
+//write file function
+
+//init function
+
+init();
---
+++ assets/lib/shapes.js
@@ -0,0 +1,16 @@
+//class for shapes
+class Shapes {
+    constructor(text, textColor, shapeColor) {
+        this.text = text;
+        this.textColor = textColor;
+        this.shapeColor = shapeColor;
+    }
+}
+
+//class for circle
+
+//class for square
+
+//class for triangle
+
+//export objects
