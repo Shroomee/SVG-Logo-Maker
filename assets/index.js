9dca0a4501993d6811ced728a8f0f8d41176b7b4
unknown <dudeomous@gmail.com>
2023-06-05 13:56:18 -0400

    added some js

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 //deconstructing objects
 const {Circle, Square, Triangle} = require('./lib/shapes');
-//objects
-
 
 //questions for user
-
+const questions = []
 //generate logo function
-
+const init = () => {
+    
+}
 //write file function
 
 //init function
--- assets/lib/shapes.js
+++ assets/lib/shapes.js
@@ -5,12 +5,28 @@ class Shapes {
         this.textColor = textColor;
         this.shapeColor = shapeColor;
     }
+
+    
 }
 
 //class for circle
-
+class Circle extends Shapes {
+    constructor(text, textColor, shapeColor) {
+        super(text, textColor, shapeColor);
+    }
+}
 //class for square
-
+class Square extends Shapes {
+    constructor(text, textColor, shapeColor) {
+        super(text, textColor, shapeColor);
+    }
+}
 //class for triangle
-
+class Triangle extends Shapes {
+    constructor(text, textColor, shapeColor) {
+        super(text, textColor, shapeColor);
+    }
+}
 //export objects
+
+module.exports = {Shapes, Circle, Square, Triangle}
