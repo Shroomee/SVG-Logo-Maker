2805010e4194a9301325d2782b8e6baef48d8d1a
unknown <dudeomous@gmail.com>
2023-06-12 15:40:42 -0400

    added some functions for testing things

--- assets/index.js
+++ assets/index.js
@@ -5,12 +5,32 @@ const inquirer = require('inquirer');
 const {Circle, Square, Triangle} = require('./lib/shapes');
 
 //questions for user
-const questions = []
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What is the text for your logo?',
+    }
+]
+
+//prompt user for input
+promptQuestions = (questions) => {
+    return inquirer.prompt(questions);
+}
 //generate logo function
 const init = () => {
     
 }
 //write file function
+const writeToFile = (fileName, content) => {
+    fs.writefile(fileName, content, err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('File created!');
+    }
+)}
+
 
 //init function
 
