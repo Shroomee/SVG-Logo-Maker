f566c76cffe4c1a47d44941675961e91b14407b4
unknown <dudeomous@gmail.com>
2023-06-12 16:34:28 -0400

    messed around with different methods

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
 // require npms
 const fs = require('fs');
 const inquirer = require('inquirer');
+const svg = require('./lib/shapes');
 //deconstructing objects
 const {Circle, Square, Triangle} = require('./lib/shapes');
 
@@ -9,29 +10,61 @@ const questions = [
     {
         type: 'input',
         name: 'text',
-        message: 'What is the text for your logo?',
-    }
+        message: 'enter at most 3 characters for your shape: ',
+        validate: (text) => {
+            if (text.length > 3) {
+                return 'too many characters';
+            }else if (text.length < 3) {
+                return 'too few characters';
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'enter a color for your text: ',
+        
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'enter a color for your shape: ',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'choose a shape: ',
+        choices: ['Circle', 'Square', 'Triangle']
+    },
 ]
 
+
+
 //prompt user for input
-promptQuestions = (questions) => {
-    return inquirer.prompt(questions);
-}
-//generate logo function
-const init = () => {
-    
-}
-//write file function
-const writeToFile = (fileName, content) => {
-    fs.writefile(fileName, content, err => {
-        if (err) {
-            console.log(err);
-        }
-        console.log('File created!');
+inquirer.prompt(questions)
+.then((answers) => {
+    console.log(answers);
+    //create new object based on user input
+    if (answers.shape === 'Circle') {
+        var newShape = new Circle(answers.text, answers.textColor, answers.shapeColor);
     }
-)}
+    if (answers.shape === 'Square') {
+        var newShape = new Square(answers.text, answers.textColor, answers.shapeColor);
+    }
+    if (answers.shape === 'Triangle') {
+        var newShape = new Triangle(answers.text, answers.textColor, answers.shapeColor);
+    }
+    //write new object to file
+    fs.writeFile('logo.svg', svg, (err) => {
+        if (err) throw err;
+        console.log('file saved');
+    }
+    )
+})
+
+    
 
 
-//init function
 
-init();
--- assets/lib/shapes.js
+++ assets/lib/shapes.js
@@ -1,32 +1,32 @@
-//class for shapes
-class Shapes {
-    constructor(text, textColor, shapeColor) {
-        this.text = text;
-        this.textColor = textColor;
-        this.shapeColor = shapeColor;
-    }
-
-    
-}
-
 //class for circle
-class Circle extends Shapes {
+var svg = ''
+class Circle {
     constructor(text, textColor, shapeColor) {
-        super(text, textColor, shapeColor);
+        var svg = 
+        `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="150" cy="100" r="100" fill="${shapeColor}" />
+            <text x="150" y="120" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+        </svg>`
     }
 }
 //class for square
-class Square extends Shapes {
+class Square {
     constructor(text, textColor, shapeColor) {
-        super(text, textColor, shapeColor);
+        var svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect x="50" height="200" width="200" fill="${shapeColor}" />
+        <text x="150" y="120" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>`
     }
 }
 //class for triangle
-class Triangle extends Shapes {
+class Triangle {
     constructor(text, textColor, shapeColor) {
-        super(text, textColor, shapeColor);
+        var svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <polygon points="150,0 275,200 25,200" fill="${shapeColor}" />
+    <text x="150" y="120" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+</svg>`
     }
 }
 //export objects
-
-module.exports = {Shapes, Circle, Square, Triangle}
+module.exports = 
+module.exports = {Circle, Square, Triangle, svg}
